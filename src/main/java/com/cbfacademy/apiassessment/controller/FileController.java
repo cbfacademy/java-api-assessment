2252b8bf94ab98439e269b7f26e54c93df62c139
Gifty Acquah <giftyacquah999@gmail.com>
2024-01-17 11:04:30 +0000

    handleFileUpload

--- src/main/java/com/cbfacademy/apiassessment/controller/FileController.java
+++ src/main/java/com/cbfacademy/apiassessment/controller/FileController.java
@@ -1,7 +1,14 @@
 package com.cbfacademy.apiassessment.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
 
 @RestController
 public class FileController {
@@ -10,5 +17,10 @@ public class FileController {
     public String test(){
         return "This is the test endpoint";
 
+    }
+
+    @GetMapping("/upload/{path}")
+    public String handleFileUpload(@PathVariable("path") String path){
+        return String.format("This is the upload endpoint %s", path) ;
     }
 }
