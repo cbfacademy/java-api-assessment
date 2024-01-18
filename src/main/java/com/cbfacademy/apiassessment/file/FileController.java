3c46ab43da117415c882d5fd259c793d347b6f18
Gifty Acquah <giftyacquah999@gmail.com>
2024-01-18 18:07:20 +0000

    Enpoints Created

--- src/main/java/com/cbfacademy/apiassessment/file/FileController.java
+++ src/main/java/com/cbfacademy/apiassessment/file/FileController.java
@@ -1,8 +1,11 @@
 package com.cbfacademy.apiassessment.file;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.multipart.MultipartFile;
@@ -26,7 +29,24 @@ public class FileController {
  
     }
 
-    //@PostMapping
-    //public String 
+    @PostMapping
+    public String uploadFile(){
 
+        return "This is the upload file endpoint";
+    }
+
+    @GetMapping
+    public String retrieveFile(){
+        return "This is the retrieve file endpoint";
+    }
+
+    @PutMapping
+    public String updateFile(){
+        return "This is the update file endpoint";
+    }
+
+    @DeleteMapping
+    public String removeFile(){
+        return "This is the delete file endpoint";
+    }
 }
