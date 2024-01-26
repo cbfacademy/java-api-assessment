6b04adbd50b8d1a60e6a1c333a9440fc4f8dfe3e
Gifty Acquah <gaatma.gl@gmail.com>
2024-01-26 18:39:45 +0000

    JSON

--- src/main/java/com/cbfacademy/apiassessment/file/FileService.java
+++ src/main/java/com/cbfacademy/apiassessment/file/FileService.java
@@ -1,20 +1,28 @@
 package com.cbfacademy.apiassessment.file;
 
+import java.io.File;
 import java.security.SecureRandom;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileService {
 
+
+    private final String JSON_FILE_PATH = "../../../../resources/uploaded_files.json";
+
+    private final String UPLOAD_DIRECTORY = "../../../../resources/uploads/";
+
     private final FileUtil fileUtil;
 
-    public FileService(FileUtil fileUtil){
+    public FileService(FileUtil fileUtil) {
 
         this.fileUtil = fileUtil;
 
@@ -25,9 +33,7 @@ public class FileService {
             String uploaderName,
             String userId) {
 
-
         // Your business logic or processing here
-
         // Variables for the uploaded file
 
         String contentType = file.getContentType();
@@ -40,8 +46,7 @@ public class FileService {
         return String.format(
                 "File '%s' uploaded by '%s' with content type '%s' and size %d bytes. Processed by service.",
                 file, uploaderName, file.getContentType(), file.getSize());
-        
-    }
 
+    }
 
 }
