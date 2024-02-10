3658f061aae7cabef8155735d96bab33e58d2932
Grace Peters <petersograce@gmail.com>
2024-02-10 19:54:36 +0000

    Getter and Setter Methods for 'Volunteer’ class

--- src/main/java/com/cbfacademy/apiassessment/volunteers/volunteer/Volunteer.java
+++ src/main/java/com/cbfacademy/apiassessment/volunteers/volunteer/Volunteer.java
@@ -2,7 +2,7 @@ package com.cbfacademy.apiassessment.volunteers.volunteer;
 
 import com.cbfacademy.apiassessment.volunteers.person.Person;
 import java.util.UUID;
-import java.util.Date;
+import java.sql.Date;
 
 
 // Volunteer specific attributes
@@ -13,7 +13,7 @@ public class Volunteer extends Person {
     private boolean active; // Indicates whther the volunteer is currently active
     private Date dateJoined; // Date when the volunteer joined the church
 
-    
+
 /**
  * Constructor for creating a new Volunteer object.
  * 
@@ -27,12 +27,57 @@ public class Volunteer extends Person {
  * @param active           Indicates whether the volunteer is currently active.
  * @param dateJoined       The date the volunteer joined the church
  */
+    
     public Volunteer(String firstName, String lastName, Date dateOfBirth, String contactNumber, String email, String role, String skills, boolean active, Date dateJoined) {
         super(firstName, lastName, dateOfBirth, contactNumber, email); // Call to super (Person) constructor
-        this.id = UUID.randomUUID(); // Generate a unique identifier for the volunteer
+        this.id = UUID.randomUUID(); // Generate a unique identifier for the volunteThe constructor Person(String, String, Date, String, String) is undefinedJava(134217858)er
         this.role = role;
         this.skills = skills;
         this.active = active;
         this.dateJoined = dateJoined;
     } 
+
+     /**
+     * Getter for the unique identifier of the volunteer.
+     *
+     * @return The UUID of the volunteer.
+     */
+    public UUID getId() {
+        return id;
+    }
+
+    // Getter and Setter methods for volunteer-specific attributes
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getDateJoined() {
+        return dateJoined;
+    }
+
+    public void setDateJoined(Date dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+
 }
