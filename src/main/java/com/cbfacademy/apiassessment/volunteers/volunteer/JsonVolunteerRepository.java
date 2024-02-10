80da09a65c0d0480070e6500661c2b88c83962f4
Grace Peters <petersograce@gmail.com>
2024-02-10 21:50:07 +0000

    chore: Add Gson for JSON serialization in VolunteerRepository

--- src/main/java/com/cbfacademy/apiassessment/volunteers/volunteer/JsonVolunteerRepository.java
+++ src/main/java/com/cbfacademy/apiassessment/volunteers/volunteer/JsonVolunteerRepository.java
@@ -1,11 +1,15 @@
 package com.cbfacademy.apiassessment.volunteers.volunteer;
 
+import com.google.gson.Gson;
 import org.springframework.stereotype.Repository;
+
+
 
 
 @Repository
 public class JsonVolunteerRepository implements VolunteerRepository {
     private final String filePath = "java-api-assessment/src/main/resources/volunteers.json";
+    private Gson gson = new Gson();
 
 
 }
