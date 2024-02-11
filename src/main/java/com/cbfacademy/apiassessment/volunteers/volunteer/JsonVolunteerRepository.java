df6cb10c67e0fa2b07b5cb6bf98526866c826f05
Grace Peters <petersograce@gmail.com>
2024-02-11 01:20:16 +0000

    Added method for populating list with initial data

--- src/main/java/com/cbfacademy/apiassessment/volunteers/volunteer/JsonVolunteerRepository.java
+++ src/main/java/com/cbfacademy/apiassessment/volunteers/volunteer/JsonVolunteerRepository.java
@@ -2,6 +2,9 @@ package com.cbfacademy.apiassessment.volunteers.volunteer;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
@@ -13,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.sql.Date;
 
 
 @Repository
@@ -54,6 +58,21 @@ private void loadVolunteers() {
             gson.toJson(volunteers, writer);  
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @PostConstruct
+    private void initVolunteersData() {
+        // Check if volunteers list is empty, indicating no data was loaded from JSON
+        if (volunteers.isEmpty()) {
+            // Populate the list with intial data
+            volunteers.add(new Volunteer("Elizabeth", "John", Date.valueOf("1980-01-15"), "07584986718", "lizzi.john@yahoo.com", "Administrator", "Organisation", true, Date.valueOf("2023-08-15")));
+            volunteers.add(new Volunteer("Matthew", "Lazarus", Date.valueOf("1962-12-07"), "07553698744", "matthewlazarus12@gmail.com", "Accountant", "Accounting", false, Date.valueOf("2012-04-25")));
+            volunteers.add(new Volunteer("Rachael", "Mark", Date.valueOf("2001-11-14"), "07956412843", "markrachael2001@live.co.uk", "Greeter", "Hospitality", true, Date.valueOf("2024-01-18")));
+            volunteers.add(new Volunteer("Joshua", "Nun", Date.valueOf("1990-01-01"), "07383555777", "j.nun@gmail.com", "Choir Member", "Singing", true, Date.valueOf("2020-01-01")));
+            volunteers.add(new Volunteer("Sarah", "Bethel", Date.valueOf("1985-05-15"), "07958444888", "bethelsarah@me.com", "Accountant", "Accounting", true, Date.valueOf("2019-05-20")));
+            // Save the newly added volunteers to the JSON file
+            saveVolunteers();
         }
     }
 
